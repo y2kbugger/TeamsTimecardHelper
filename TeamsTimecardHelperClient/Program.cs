42935f0bb7a8c9de833bafd583d596694fd46e81
y2k <git@y2kbugger.com>
2022-11-23 18:17:44 -0500

    Rename client to TeamsTimecardHelperClient

--- TeamsTimecardHelperClient/Program.cs
+++ TeamsTimecardHelperClient/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TeamsTimecardHelper;
+using TeamsTimecardHelperClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
