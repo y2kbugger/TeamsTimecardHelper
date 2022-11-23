4bf9a909d1e7c1dccef8e88e235eaeab277a3bce
y2k <git@y2kbugger.com>
2022-11-23 18:44:37 -0500

    First stab at graph api demo

--- TeamsTimecardHelperClient/Program.cs
+++ TeamsTimecardHelperClient/Program.cs
@@ -11,5 +11,9 @@ builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
 });
+builder.Services.AddGraphClient(
+    "https://graph.microsoft.com/User.Read",
+    "https://graph.microsoft.com/Schedule.ReadWrite.All"
+    );
 
 await builder.Build().RunAsync();
