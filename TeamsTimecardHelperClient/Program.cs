0c4cd15e2f082f49a9a6362c15df75203b6bf8ca
y2k <git@y2kbugger.com>
2022-11-23 19:47:57 -0500

    Change formatting in Program.cs

--- TeamsTimecardHelperClient/Program.cs
+++ TeamsTimecardHelperClient/Program.cs
@@ -7,10 +7,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMsalAuthentication(options =>
-{
+builder.Services.AddMsalAuthentication(options => {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-});
+    });
 builder.Services.AddGraphClient(
     "https://graph.microsoft.com/User.Read",
     "https://graph.microsoft.com/Schedule.ReadWrite.All"
