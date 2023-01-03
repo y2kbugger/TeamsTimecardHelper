17d5871e8b5e38c15a80c0edd47bda7a27a3715e
y2k <git@y2kbugger.com>
2023-01-02 21:44:52 -0500

    Cache the teamid in LocalStorage

--- TeamsTimecardHelperClient/Program.cs
+++ TeamsTimecardHelperClient/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TeamsTimecardHelperClient;
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMsalAuthentication(options => {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
