65fca8b4a5d0e2ef53ffc19773b467e7cc5a769e
y2k <git@y2kbugger.com>
2023-01-25 12:25:15 -0500

    Don't redraw whole page on ticker redraw

---
+++ TeamsTimecardHelperClient/TimeCardDuration.cs
@@ -0,0 +1,49 @@
+using Microsoft.Graph;
+
+static class TimeCardDuration
+{
+    public static string FormatDuration(TimeSpan? ts)
+    {
+        if (ts == null)
+            return "--:--:--";
+        var tss = (TimeSpan)ts;
+        return $"{(tss.Days * 24) + tss.Hours:00}:{tss.Minutes:00}:{tss.Seconds:00}";
+    }
+
+    public static TimeSpan? TotalTimespan(IEnumerable<TimeCard>? timeCards)
+    {
+        if (timeCards == null)
+            return null;
+
+        var total = new TimeSpan();
+        foreach (var tc in timeCards)
+        {
+            total += HoursFromTimeCard(tc);
+        }
+
+        // This is required to stop blazor from infinitely re-rendering the page
+        return new TimeSpan(total.Days, total.Hours, total.Minutes, total.Seconds);
+    }
+
+
+    public static TimeSpan HoursFromTimeCard(TimeCard tc)
+    {
+            DateTime dt_in = tc!.ClockInEvent?.DateTime?.DateTime ?? throw new Exception("TimeCard.ClockInEvent.DateTime should never not be null");
+            DateTime dt_out = tc?.ClockOutEvent?.DateTime?.DateTime ?? DateTime.UtcNow; // Still clocked in
+            var hours = (dt_out - dt_in);
+
+            foreach (var b in tc?.Breaks ?? Enumerable.Empty<TimeCardBreak>())
+                hours  -=  HoursFromTimeCardBreak(b);
+
+            return hours;
+    }
+
+    public static  TimeSpan HoursFromTimeCardBreak(TimeCardBreak tcb)
+    {
+        DateTime dt_in = tcb!.Start?.DateTime?.DateTime ?? throw new Exception("TimeCardBreak.Start.DateTime should never not be null");
+        DateTime dt_out = tcb?.End?.DateTime?.DateTime ?? DateTime.UtcNow; // Still on break
+        var hours = (dt_out - dt_in);
+        return hours;
+    }
+
+}
