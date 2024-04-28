c40698fbec99bd7fade23731bd95d6ff5ad3e3db
y2k <git@y2kbugger.com>
2024-04-27 22:22:55 -0400

    Add options for hours and summary stats

--- TeamsTimecardHelperClient/ClientSettings.cs
+++ TeamsTimecardHelperClient/ClientSettings.cs
@@ -11,4 +11,26 @@ internal class ClientSettings
         get => _localstorage.GetItem<string>("theteamid");
         set =>_localstorage.SetItem("theteamid", value);
     }
+
+    public DateOnly statsstartdate {
+        get {
+            string date_stamp = _localstorage.GetItem<string>("statsstartdate");
+            try {
+                return DateOnly.FromDateTime(DateTime.Parse(date_stamp));
+            }
+            catch (Exception)
+            {
+                return new DateOnly(DateTime.Now.Year, 1, 1);
+            }
+        }
+        set {
+            string date_stamp = value.ToString();
+            _localstorage.SetItem("statsstartdate", date_stamp);
+        }
+    }
+
+    public int targetweeklyhours {
+        get => _localstorage.GetItem<int>("targetweeklyhours");
+        set => _localstorage.SetItem("targetweeklyhours", value);
+    }
 }
